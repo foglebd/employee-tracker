81c6dce8dba394dc3883c0069a97ec19fb61a603
Blake Fogle <foglebd@gmail.com>
2019-12-17 18:08:45 -0800

    terms and variables updated

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,5 +17,77 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    console.log("connected as id " + connection.threadId + "\n");
+    addEmployee();
 });
+
+function addEmployee() {
+    console.log("Adding a new Employee...\n");
+    var query = connection.query(
+        "INSERT INTO employee SET ?",
+        {
+            first_name: "James",
+            last_name: "Brown",
+            role_id: 2,
+            manager_id: 0
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee added!\n");
+            updateEmployee();
+        }
+    );
+
+    // logs the actual query being run
+    console.log(query.sql);
+}
+
+function updateEmployee() {
+    console.log("Updating employee...\n");
+    var query = connection.query(
+        "UPDATE employee SET ? WHERE ?",
+        [
+            {
+                role_id: 1
+            },
+            {
+                last_name: "Brown"
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee updated!\n");
+
+            removeEmployee();
+        }
+    );
+
+    // logs the actual query being run
+    console.log(query.sql);
+}
+
+function removeEmployee() {
+    console.log("Removing employee from database...");
+    connection.query(
+        "DELETE FROM database WHERE ?",
+        {
+            id: 2
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee removed!\n");
+
+            readEmployee();
+        }
+    );
+}
+
+function readEmployee() {
+    console.log("Selecting all employees in database...\n");
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+    });
+}
